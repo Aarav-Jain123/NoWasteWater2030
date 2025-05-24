50cda10c4313c636ae70b5cc73dda459c605a40d
Aarav Jain <137067931+Aarav-Jain123@users.noreply.github.com>
2025-05-24 22:38:09 +0530

    frontend setup

---
+++ frontendApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+  <Navbar/>
+    </>
+  )
+}
+
+export default App;
